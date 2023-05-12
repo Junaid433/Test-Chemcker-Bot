68d9b24fffb4fc39ba758d79e32ee0459158c8fb
Cj 1977 <132664743+CjTupac1977@users.noreply.github.com>
2023-05-12 16:56:22 +0300

    Update functions.php

--- Core/functions.php
+++ Core/functions.php
@@ -24,7 +24,7 @@ function multiexplode($delimiters, $string)
 }
 
 function editMessage ($chatId, $message,$message_id){
-$url = "https://api.telegram.org/bot5980177112:AAF2saZaYIej03yT5_UE3Gbjlpgi1g7tnyw/editMessageText?chat_id=".$chatId."&message_id=".$message_id."&text=".$message."&parse_mode=HTML";
+$url = "https://api.telegram.org/bot6156162011:AAFpad9_GzesePHMO29kD_0M2uqwR3LUHgs/editMessageText?chat_id=".$chatId."&message_id=".$message_id."&text=".$message."&parse_mode=HTML";
 file_get_contents($url);      
 }
 
